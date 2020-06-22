26cb5c6475ac0394539f5419f978e7a1ac2cafd8
Miguel Angel Bacigaluppi <miguebaci@gmail.com>
2020-06-22 16:58:33 -0300

    minor fixes

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -11,39 +11,44 @@ public class Client {
 
     public static void main(String args[]) throws Exception {
 
-    	try {
-    		String host;
-    		Integer port;
-			Scanner scanner = new Scanner(System.in);
-			System.out.print("Please insert the host IP: ");
-			host = scanner.nextLine();
-			System.out.print("Please insert the port number: ");
-			port = scanner.nextInt();
-			Socket socket = new Socket(host, port);
-			BufferedReader dataIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintStream sout = new PrintStream(socket.getOutputStream());
-			BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-			String msg;
-			System.out.print("Connected \n");
-			while (true) {
-				System.out.print("Client : ");
-				msg = stdin.readLine();
-				sout.println(msg);
-				if (msg.equalsIgnoreCase("x")) {
-					System.out.println("Connection ended by client");
-					break;
-				}
-				msg = dataIn.readLine();
-				System.out.print("Server : " + msg + "\n");
+        try {
+            String host;
+            Integer port;
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Please insert the host IP: ");
+            host = scanner.nextLine();
+            System.out.print("Please insert the port number: ");
+            port = scanner.nextInt();
+            //con los datos ingresados se instancia un socket
+            Socket socket = new Socket(host, port);
+            BufferedReader dataIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintStream sout = new PrintStream(socket.getOutputStream());
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            String msg;
+            System.out.print("Connected \n");
+            while (true) {
+                //si se puede conectar espera a que el cliente ingrese un mensaje
+                System.out.print("Client : ");
+                msg = stdin.readLine();
+                sout.println(msg);
+                //si el mensaje enviado por el cliente es x se rompe el loop y se cierran los sockets
+                if (msg.equalsIgnoreCase("x")) {
+                    System.out.println("Connection ended by client");
+                    break;
+                }
+                //se lee el mensaje enviado por el servidor
+                msg = dataIn.readLine();
+                System.out.print("Server : " + msg + "\n");
 
-			}
-			socket.close();
-			dataIn.close();
-			sout.close();
-			stdin.close();
-		}catch (IOException e){
-			System.out.println("Error: can't connect to server");
-		}
+            }
+            //se cierra el socket y los buffers
+            socket.close();
+            dataIn.close();
+            sout.close();
+            stdin.close();
+        } catch (IOException e) {
+            System.out.println("Error: can't connect to server");
+        }
     }
 
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -25,16 +24,20 @@ public class Server {
 
     Server(int port) {
         this.port = port;
+        //se crea un pool para que hasta 5 clientes puedan conectarse al servidor al mismo tiempo
         pool = Executors.newFixedThreadPool(5);
     }
 
     public void startServer() throws IOException {
 
-        server = new ServerSocket(3000);
+        //se inicia el servidor
+        server = new ServerSocket(this.port);
         System.out.println("Server Booted");
         while (true) {
+            //este loop eterno hace que se este constantemente buscando nuevas conexiones al socket del servidor
             client = server.accept();
             clientcount++;
+            //Se crea un hilo con el scocket del cliente y se lo añade al pool
             ServerThread runnable = new ServerThread(client, clientcount, this);
             pool.execute(runnable);
         }
@@ -48,7 +51,7 @@ public class Server {
         BufferedReader cin;
         PrintStream cout;
         int id;
-        String s;
+        String msg;
 
         ServerThread(Socket client, int count, Server server) throws IOException {
 
@@ -66,25 +69,30 @@ public class Server {
         public void run() {
             try {
                 while (true) {
-                    s = cin.readLine();
-
-                    System.out.print("Client(" + id + ") :" + s + "\n");
-
-                    if (s.equalsIgnoreCase("x")) {
+                    //lee el mensaje enviado por el cliente
+                    msg = cin.readLine();
+                    //lo muestra por pantalla
+                    System.out.print("Client(" + id + ") :" + msg + "\n");
+                    //si el mensaje del cliente es x se descuenta un cliente del contador de clientes, cuando llega a 0 el servidor se cierra
+                    if (msg.equalsIgnoreCase("x")) {
                         cout.println("BYE");
                         clientcount--;
                         System.out.println("Connection ended by client " + id);
                         break;
                     }
+                    //mensaje de confirmacion enviado por el servidor al cliente, este se mostrara en pantalla al cliente
                     cout.println("Message received by server");
                 }
+                //cuando llega a 0 el servidor se cierra
                 if (clientcount == 0) {
                     System.out.println("Server cleaning up.");
+                    //Se cierran los sockets y buffers
+                    cin.close();
+                    client.close();
+                    cout.close();
                     System.exit(0);
                 }
-                cin.close();
-                client.close();
-                cout.close();
+
             } catch (IOException ex) {
                 System.out.println("Error : " + ex);
                 try {
@@ -92,7 +100,7 @@ public class Server {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }catch (NullPointerException ex){
+            } catch (NullPointerException ex) {
                 System.out.println("Error : " + ex);
                 try {
                     client.close();
